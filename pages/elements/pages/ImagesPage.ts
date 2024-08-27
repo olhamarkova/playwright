b344bbaa3a2a0393498cf371505544e91d8a4c5c
olhamarkova <olga.mulchenko@gmail.com>
2024-08-27 19:26:09 +0300

    add test for broken links

--- pages/elements/pages/ImagesPage.ts
+++ pages/elements/pages/ImagesPage.ts
@@ -1,9 +1,12 @@
-import { type Page, Locator } from "@playwright/test";
+import { type Page, Locator, expect } from "@playwright/test";
 import { InnerPage } from "../../core/InnerPage";
 
 export class ImagesPage extends InnerPage {
   readonly validImage: Locator;
   readonly brokenImage: Locator;
+  readonly images: Locator;
+  readonly validLink: Locator;
+  readonly links: Locator;
 
   constructor(page: Page, url: string) {
     super(page, url);
@@ -11,9 +14,44 @@ export class ImagesPage extends InnerPage {
       .locator("img[src='/images/Toolsqa.jpg']")
       .nth(1);
     this.brokenImage = this.page.locator("img[src='/images/Toolsqa_1.jpg']");
+    this.images = this.page.locator("div#app img");
+    this.links = this.page.locator("div#app").getByRole("link");
   }
 
   link(linkText: string) {
     return this.page.getByRole("link", { name: linkText, exact: true });
+  }
+
+  async findBrokenImage() {
+    const url = `${process.env.URL}`.slice(0, -1);
+    const allImages = await this.images.all();
+    for await (const image of allImages) {
+      const imageSrc = await image.getAttribute("src");
+      expect.soft(imageSrc?.length).toBeGreaterThan(1);
+      //@ts-ignore
+      if (imageSrc?.length > 0) {
+        const response = await this.page.request.get(`${url}${imageSrc}`);
+        expect.soft(response.status()).toBe(200);
+        await this.page.goto(`${url}${imageSrc}`);
+        const title = await this.page.title();
+        if (title === "DEMOQA") {
+          throw new Error(`Here is a broken image: ${url}${imageSrc}`);
+        }
+        await this.page.goBack();
+      }
+    }
+  }
+
+  async findBrokenLink() {
+    const allLinks = await this.links.all();
+    for (const link of allLinks) {
+      const linkHref = await link.getAttribute("href");
+      const response = await this.page.request.get(linkHref!);
+      if (response.status() === 500) {
+        throw new Error(`This is a broken link: ${linkHref}`);
+      } else {
+        expect.soft(response).toBeOK();
+      }
+    }
   }
 }
--- tests/elements/images.spec.ts
+++ tests/elements/images.spec.ts
@@ -1,11 +1,8 @@
-import { test, expect } from "@playwright/test";
-import { subCategoriesUrls, title } from "../../utils/services/dataService.ts";
+import { test } from "@playwright/test";
+import { subCategoriesUrls } from "../../utils/services/dataService.ts";
 import {
   elementPagesHeadings as headings,
-  linkNames,
   linksText,
-  requestLinks,
-  subHeadings,
   subHeadingsText,
 } from "../../pages/elements/elementsData.ts";
 import { ImagesPage } from "../../pages/elements/pages/ImagesPage.ts";
@@ -17,10 +14,8 @@ test.beforeEach(async ({ page }) => {
   await imagesPage.visit();
 });
 
-test.describe("Links Page Tests", () => {
-  test("@smoke The Images And Links Page Should Have All The Expected Elements", async ({
-    page,
-  }) => {
+test.describe("Images And Links Page Tests", () => {
+  test("@smoke The Images And Links Page Should Have All The Expected Elements", async () => {
     await test.step("Step 1: Check The Page Headings", async () => {
       await imagesPage.validateHeading(headings.images);
       for (let i = 0; i < subHeadingsText.length; i++) {
@@ -43,28 +38,15 @@ test.describe("Links Page Tests", () => {
     });
   });
 
-  test("@smoke Find the broken image", async ({ page }) => {
+  test("@negative Validate Images", async () => {
     await test.step("Step 1: Find the broken image", async () => {
-      const images = page.locator("div#app img");
-      const allImages = await images.all();
-      for await (const image of allImages) {
-        const imageSrc = await image.getAttribute("src");
-        expect.soft(imageSrc?.length).toBeGreaterThan(1);
-        //@ts-ignore
-        if (imageSrc?.length > 0) {
-          const response = await page.request.get(
-            `https://demoqa.com${imageSrc}`
-          );
-          expect.soft(response.status()).toBe(200);
-          await page.goto(`https://demoqa.com${imageSrc}`);
-          await expect.soft(page.locator(`img`)).toBeVisible();
-          const title = await page.title();
-          if (title === "DEMOQA") {
-            throw new Error("Here is a broken image");
-          }
-          await page.goBack();
-        }
-      }
+      await imagesPage.findBrokenImage();
+    });
+  });
+
+  test("@negative Validate Links", async () => {
+    await test.step("Step 1: Find The Broken Link", async () => {
+      await imagesPage.findBrokenLink();
     });
   });
 });
