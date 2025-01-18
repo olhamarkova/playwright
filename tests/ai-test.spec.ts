5019ad6342401e7e144e0797815123bf1fa2ef82
olhamarkova <olga.mulchenko@gmail.com>
2025-01-18 22:52:28 +0200

    fix ai test

--- tests/ai-test.spec.ts
+++ tests/ai-test.spec.ts
@@ -1,37 +1,37 @@
 //Experimental test using AI
 
-import { aiTest } from "../fixtures/ai-fixture";
+import { aiTest as test } from "../fixtures/ai-fixture";
 
-aiTest.describe("AI Powered Test", async () => {
-  aiTest("Simple AI Test", async ({ ai, page }) => {
+test.describe("AI Powered Test", async () => {
+  test("Simple AI Test", async ({ ai, page }) => {
     await page.goto(`${process.env.URL}webtables`);
 
-    aiTest.step("Count table rows", async () => {
+    await test.step("Count table rows", async () => {
       const countOfRows = await ai(
         "How many rows with data does the table have?"
       );
       console.log(`The table has ${countOfRows} row(s)`); //Expected output: '3'
     });
 
-    aiTest.step("Check the department", async () => {
+    await test.step("Check the department", async () => {
       const department = await ai(
         "What department does Kierra Gentry work at?"
       );
       console.log(`The Kierra's department is ${department}`); //Expected output: 'Legal'
     });
 
-    aiTest.step("Open the registration form", async () => {
+    await test.step("Open the registration form", async () => {
       await ai("Click the 'Add' button");
       const heading = await ai("Get the text of the modal heading");
       console.log(heading); //Expected output: 'Registration Form'
     });
 
-    aiTest.step("Fill out the form", async () => {
+    await test.step("Fill out the form", async () => {
       await ai("Fill out the form with realistic values");
       await ai("Click the 'Submit' button");
     });
 
-    aiTest.step("Count table rows", async () => {
+    await test.step("Count table rows", async () => {
       const countOfRowsAfter = await ai(
         "How many rows with data does the table have?"
       );
